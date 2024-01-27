79b52472aaf146eb064caf6f7b0f3546a4681130
Linghua Jin <linghua.jin@gmail.com>
2024-01-26 23:02:29 -0800

    Enable logs...
    got Requested application data is not configured correctly. View/enable debug logs for more details

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ use crate::handlers::*;
 // specify main function so it runs in the Actix run time (rt)
 #[actix_rt::main]
 async fn main()-> io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    
     // load server configs
     dotenv().ok();
     let config = Config::from_env().unwrap();
